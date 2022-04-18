a1a4abde121577f4a73e2e0b8cfd63e21ff4a885
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-04-18 16:57:02 +0530

    enhance: handle invalid value error

--- main.py
+++ main.py
@@ -5,30 +5,36 @@ import string
 strength = 8
 
 def generatePassword(strength):
-    alphabets = string.ascii_letters + string.digits
-    password = ""
+  alphabets = string.ascii_letters + string.digits
+  password = ""
 
-    for alphabet in alphabets:
-        strength -= 1
-        
-        if (strength == 0):
-            break
+  for alphabet in alphabets:
+    strength -= 1
     
-        password += choice(alphabets)
+    if (strength == 0):
+      break
 
-    return password
+    password += choice(alphabets)
+
+  return password
 
 if __name__ == "__main__":
-    print("""
-     ____
-    |  _ \ __ _ ___ ___  __ _  ___ _ __
-    | |_) / _` / __/ __|/ _` |/ _ \ '_ \\ 
-    |  __/ (_| \__ \__ \ (_| |  __/ | | |
-    |_|   \__,_|___/___/\__, |\___|_| |_|
-                        |___/
-    """)
-    
-    strength = int(input("Enter Strength for the password: "))
-    generated = generatePassword(strength)
+  print("""
+   ____
+  |  _ \ __ _ ___ ___  __ _  ___ _ __
+  | |_) / _` / __/ __|/ _` |/ _ \ '_ \\ 
+  |  __/ (_| \__ \__ \ (_| |  __/ | | |
+  |_|   \__,_|___/___/\__, |\___|_| |_|
+                      |___/
+  """)
+  
+  try:
+    strength = int(input("How strong do you want your password?"))
 
-    print(f"Generated passwdord: {generated}")
+  except ValueError:
+    print("Please enter a number")
+    exit()
+
+  generated = generatePassword(strength)
+
+  print(f"Generated passwdord: {generated}")
